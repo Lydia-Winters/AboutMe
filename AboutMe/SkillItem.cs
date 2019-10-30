007f6a0f7eb1ab2f39ee5c3aee670c46f74d47f5
lydiawinters <lrwinters@gus.pittstate.edu>
2019-10-30 18:10:30 -0500

    * AboutMe.Android.csproj: skills
    * SkillItem.cs:
    * SkillsPage.xaml:
    * SkillsPage.xaml.cs:
    * Resource.designer.cs:
    * codeicon.png:
    * clockicon.png:
    * sewingicon.png:
    * graphicicon.png:

---
+++ AboutMe/SkillItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AboutMe
+{
+    public class SkillItem
+    {
+       
+        public string Icon { get; set; }
+        public string Name { get; set; }
+    }
+   
+    }
+
--- AboutMe/SkillsPage.xaml.cs
+++ AboutMe/SkillsPage.xaml.cs
@@ -1,15 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace AboutMe
 {
     public partial class SkillsPage : ContentPage
     {
+        public ObservableCollection<SkillItem>Skills {get; set; }
+
         public SkillsPage()
         {
             InitializeComponent();
+
+            this.Skills = new ObservableCollection<SkillItem>();
+
+            SkillItem WebSkill = new SkillItem();
+            WebSkill.Name = "Web Development";
+            WebSkill.Icon = "codeicon.png";
+
+            SkillItem GraphicsSkill = new SkillItem();
+            GraphicsSkill.Name = "Graphic Design";
+            GraphicsSkill.Icon = "graphicicon.png";
+
+            SkillItem SewingSkill = new SkillItem();
+            SewingSkill.Name = "Sewing & Crafting";
+            SewingSkill.Icon = "sewingicon.png";
+
+            SkillItem TimeSkill = new SkillItem();
+            TimeSkill.Name = "Time Managenment";
+            TimeSkill.Icon = "timeicon.png";
+
+            Skills.Add(WebSkill);
+            Skills.Add(GraphicsSkill);
+            Skills.Add(SewingSkill);
+            Skills.Add(TimeSkill);
+
+            skillsListView.ItemsSource = this.Skills;
+
         }
+
+       
     }
 }
